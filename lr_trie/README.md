d682b1a5901e80ee92eaec493e91be680eb92650
Mr. Deepest Outcome <uzivatel919@seznam.cz>
2025-03-23 21:33:53 +0100

    readme update

--- lr_trie/README.md
+++ lr_trie/README.md
@@ -3,12 +3,36 @@
 Left-Right trie is trie that allows mapping of any string to any string with complexity based on alphabet used size.
 
 ```rust
-let mut trie = LrTrie::new();
-let one = KeyEntry::new("emoción").unwrap();
-let another = KeyEntry::new("emotion").unwrap();
+const ONE: &str = "emoción";
+const ANOTHER: &str = "emotion";
+const REPLACEMENT: &str = "equilibrio sicofísico";
 
-trie.insert(&one, &another);
-assert!(trie.member(&one, LeftRight::Left).is_some());
-assert!(trie.member(&another, LeftRight::Left).is_none());
-assert!(trie.member(&another, LeftRight::Right).is_some());
+let mut trie = LrTrie::new();
+
+let one = &KeyEntry::new(ONE).unwrap();
+let another = &KeyEntry::new(ANOTHER).unwrap();
+let replacement = &KeyEntry::new(REPLACEMENT).unwrap();
+
+trie.insert(one, another);
+assert!(trie.member(one, LeftRight::Left).is_some());
+assert!(trie.member(another, LeftRight::Left).is_none());
+assert!(trie.member(another, LeftRight::Right).is_some());
+
+trie.insert(one, replacement);
+assert_eq!(
+    Some(String::from(REPLACEMENT)),
+    trie.member(&one, LeftRight::Left)
+);
+assert!(trie.member(another, LeftRight::Right).is_none());
+assert_eq!(
+    Some(String::from(ONE)),
+    trie.member(replacement, LeftRight::Right)
+);
+
+assert_eq!(Ok(()), trie.delete(one, LeftRight::Left));
+
+assert!(trie.member(one, LeftRight::Left).is_none());
+assert!(trie.member(replacement, LeftRight::Right).is_none());
+
+assert_eq!(Err(()), trie.delete(replacement, LeftRight::Right));
 ```
--- lr_trie/src/lib.rs
+++ lr_trie/src/lib.rs
@@ -2070,14 +2070,38 @@ mod tests_of_units {
 
         #[test]
         fn test() {
-            let mut trie = LrTrie::new();
-            let one = KeyEntry("emoción");
-            let another = KeyEntry("emotion");
+            const ONE: &str = "emoción";
+            const ANOTHER: &str = "emotion";
+            const REPLACEMENT: &str = "equilibrio sicofísico";
 
-            trie.insert(&one, &another);
-            assert!(trie.member(&one, LeftRight::Left).is_some());
-            assert!(trie.member(&another, LeftRight::Left).is_none());
-            assert!(trie.member(&another, LeftRight::Right).is_some());
+            let mut trie = LrTrie::new();
+
+            let one = &KeyEntry::new(ONE).unwrap();
+            let another = &KeyEntry::new(ANOTHER).unwrap();
+            let replacement = &KeyEntry::new(REPLACEMENT).unwrap();
+
+            trie.insert(one, another);
+            assert!(trie.member(one, LeftRight::Left).is_some());
+            assert!(trie.member(another, LeftRight::Left).is_none());
+            assert!(trie.member(another, LeftRight::Right).is_some());
+
+            trie.insert(one, replacement);
+            assert_eq!(
+                Some(String::from(REPLACEMENT)),
+                trie.member(&one, LeftRight::Left)
+            );
+            assert!(trie.member(another, LeftRight::Right).is_none());
+            assert_eq!(
+                Some(String::from(ONE)),
+                trie.member(replacement, LeftRight::Right)
+            );
+
+            assert_eq!(Ok(()), trie.delete(one, LeftRight::Left));
+
+            assert!(trie.member(one, LeftRight::Left).is_none());
+            assert!(trie.member(replacement, LeftRight::Right).is_none());
+
+            assert_eq!(Err(()), trie.delete(replacement, LeftRight::Right));
         }
     }
 }
