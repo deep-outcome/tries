9a7aa8d02a68c29f73c1177ce3d81aea0f2bb616
Mr. Deepest Outcome <uzivatel919@seznam.cz>
2025-07-06 19:19:31 +0200

    build script

---
+++ poetrie/build.rs
@@ -0,0 +1,18 @@
+use std::fs::copy;
+use std::path::Path;
+
+fn main() {
+    cp("uc");
+}
+
+fn cp(load: &str) {
+    let dir = format!("./src/{}", load);
+    _ = std::fs::create_dir(dir);
+
+    let src = format!("../{}/src/lib.rs", load);
+    let dst = format!("./src/{}/mod.rs", load);
+
+    let src = Path::new(src.as_str());
+    let dst = Path::new(dst.as_str());
+    _ = copy(src, dst);
+}
